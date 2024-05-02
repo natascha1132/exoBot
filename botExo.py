58e13c9a927b4712c491f366d9568e5542872d61
natascha1132 <natascha1132@gmail.com>
2024-05-02 12:25:27 +0300

    new

--- botExo.py
+++ botExo.py
@@ -1,21 +1,26 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
-from dotenv import load_dotenv, find_dotenv
-import os
-import logging
 
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = '7057532692:AAHwUixvbpFE5Zu3mcDAWIXAlkzIhvbQcvo'
+
+
+# Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=["start"]))
+
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nЭто ЭХО"!\nНапиши мне')
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -23,15 +28,55 @@ async def process_help_command(message: Message):
     )
 
 
+
+
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+async def send_video_echo(message: Message):
+    await message.reply_video(message.video.file_id)
+
+async def send_audio_echo(message: Message):
+    await message.reply_audio(message.audio.file_id)
+
+async def send_voice_echo(message: Message):
+    await message.reply_voice(message.voice.file_id)
+
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+async def send_video_note_echo(message: Message):
+    await message.reply_video_note(message.video_note.file_id)
+
+async def send_gif_echo(message: Message):
+    await message.reply_gif(message.gif.file_id)
+async def send_document_echo(message: Message):
+    await message.reply_document(message.document.file_id)
+
+async def send_emoji_echo(message: Message):
+    await message.reply_emoji(message.emoji.file_id)
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
-@dp.message(F.sticker)
-async def get_sticker(message: Message):
-    await message.answer('Стикер классный! Жаль такого у меня нет :(')
+
+
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_voice_echo, F.voice)
+dp.message.register(send_audio_echo, F.audio)
+dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_video_note_echo, F.video_note)
+dp.message.register(send_document_echo, F.document)
+dp.message.register(send_emoji_echo, F.emoji)
+dp.message.register(send_echo)
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
